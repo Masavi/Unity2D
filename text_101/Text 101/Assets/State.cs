1a2feb09207b8ec83054a9429a293750555e617a
Masavi <masavi2008@hotmail.com>
2018-08-15 17:13:44 -0500

    Texto 101 pt. 1

---
+++ text_101/Text 101/Assets/State.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject {
+
+    // Adds default field size for storyText
+    [TextArea(14, 10)] [SerializeField] string storyText;
+    [SerializeField] State[] nextStates;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
+}
